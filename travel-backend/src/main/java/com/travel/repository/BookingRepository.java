df84d5130b1559cbd0c2c2c110fb3f396518f77b
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2026-07-30 14:49:50 +0530

    Delete travel-backend directory

--- travel-backend/src/main/java/com/travel/repository/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.travel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.travel.entity.Booking;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
--- travel-backend/src/main/java/com/travel/repository/ContactRepository.java
+++
@@ -1,8 +0,0 @@
-package com.travel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.travel.entity.Contact;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
